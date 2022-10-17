1323a0acc934a21f882415519c2b9a2de14392f8
Saif <saif-alvi@hotmail.com>
2022-10-16 22:09:48 -0400

    Complete first versionrock paper scissors untested

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+let getComputerChoice = () => {let x =Math.random()
+    if (x < 0.33){
+        return 'Rock';
+    }
+    else if(x>=0.66){
+        return 'Paper';
+    }
+    else{
+        return 'Scissors';
+    }
+}
+
+let playRound = (playerSelection, computerSelection) => { 
+
+    let x =playerSelection.substr(0,1).toUpperCase() + playerSelection(1).toLowerCase();
+
+    let y = computerSelection;
+
+    if (x == 'Rock' && y == 'Scissors' || x == 'Paper' && y == 'Rock' || x == 'Scissors' && y == 'Paper' ){
+        return `You win! ${x} beats ${y}`
+    }
+    else{
+        return `You lose! ${y} beats ${x}`
+    }
+}
+
+function Game(){
+
+    for (let i = 0; i < 5; i++){
+        playRound(window.prompt("Do you choose Rock, Paper or Scissors?"),getComputerChoice());
+    }
+
+}
