f0e41f9bf9fe2feb9981184e1889f167840b4ba5
Saif <saif-alvi@hotmail.com>
2022-10-28 19:02:15 -0400

    Add working buttons that print correct to console

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button)=>{
+button.addEventListener('click', SelectionMadeStarter);
+})
 
 
+function SelectionMadeStarter(event){
+    console.log(event.target);
+    console.log(playRound(event.target.textContent,getComputerChoice()));
+}
 
 
+let playRound = (playerSelection, computerSelection) => { 
+
+    let x = playerSelection.substr(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
+    let y = computerSelection;
+
+    if (x == 'Rock' && y == 'Scissors' || x == 'Paper' && y == 'Rock' || x == 'Scissors' && y == 'Paper' ){
+        return `You win! ${x} beats ${y}.`
+    }
+    else if (x == y){
+        return `Tie! ${y} and ${x} are the same.`
+    }
+    else{
+        return `You lose! ${y} beats ${x}.`
+    }
+}
 
 
 let getComputerChoice = () => {let x =Math.random()
@@ -16,25 +40,14 @@ let getComputerChoice = () => {let x =Math.random()
     }
 }
 
-let playRound = (playerSelection, computerSelection) => { 
-
-    let x =playerSelection.substr(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
-
-    let y = computerSelection;
-
-    if (x == 'Rock' && y == 'Scissors' || x == 'Paper' && y == 'Rock' || x == 'Scissors' && y == 'Paper' ){
-        return `You win! ${x} beats ${y}`
-    }
-    else{
-        return `You lose! ${y} beats ${x}`
-    }
-}
 
 function Game(){
 
-    for (let i = 0; i < 5; i++){
-        console.log(playRound(window.prompt("Do you choose Rock, Paper or Scissors?"),getComputerChoice()));
-        
-    }
-
+    console.log(playRound(window.prompt("Do you choose Rock, Paper or Scissors?"),getComputerChoice()));    
+    console.log("Game Over.")
 }
+
+
+
+
+
