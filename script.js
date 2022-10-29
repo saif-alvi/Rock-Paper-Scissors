b671f9f2a3d12e6b1af25c3f9a6c50b521e987c8
Saif <saif-alvi@hotmail.com>
2022-10-28 20:16:25 -0400

    Complete UI functionallity with updating score board and win result display

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-let buttons = document.querySelectorAll('button');
+let playerScore=0;
+let computerScore=0;
 
-buttons.forEach((button)=>{
-button.addEventListener('click', SelectionMadeStarter);
-})
+const display = document.querySelector('#container');
+const buttons = document.querySelectorAll('button');
+    buttons.forEach((button)=> button.addEventListener('click', SelectionMadeStarter));
+const currentPlay= document.createElement('p');
+    display.appendChild(currentPlay);
+const playerScoreBoard= document.createElement('p');
+    playerScoreBoard.textContent=`Player = ${playerScore}`
+const computerScoreBoard= document.createElement('p');
+    computerScoreBoard.textContent=`Computer = ${computerScore}`
+    display.appendChild(playerScoreBoard)
+    display.appendChild(computerScoreBoard)
+const result = document.createElement('p');
+    display.appendChild(result)
 
 
 function SelectionMadeStarter(event){
+    result.textContent=''
     console.log(event.target);
     console.log(playRound(event.target.textContent,getComputerChoice()));
+    
+    if (playerScore == 5){
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreBoard.textContent=`Player = ${playerScore}`
+        computerScoreBoard.textContent=`Computer = ${computerScore}`
+        result.textContent='Game Over!, The Player wins!'
+    }
+    else if (computerScore == 5){
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreBoard.textContent=`Player = ${playerScore}`
+        computerScoreBoard.textContent=`Computer = ${computerScore}`
+        result.textContent='Game Over!, The Computer wins!'
+    }
+
+    
 }
+
+///Helpers -----------------
 
 
 let playRound = (playerSelection, computerSelection) => { 
-
     let x = playerSelection.substr(0,1).toUpperCase() + playerSelection.substr(1).toLowerCase();
     let y = computerSelection;
 
     if (x == 'Rock' && y == 'Scissors' || x == 'Paper' && y == 'Rock' || x == 'Scissors' && y == 'Paper' ){
-        return `You win! ${x} beats ${y}.`
+        playerScore+=1;
+        playerScoreBoard.textContent=`Player = ${playerScore}`
+        currentPlay.textContent=`You win! ${x} beats ${y}.`
     }
     else if (x == y){
-        return `Tie! ${y} and ${x} are the same.`
+        currentPlay.textContent=`Tie! ${y} and ${x} are the same.`
     }
     else{
-        return `You lose! ${y} beats ${x}.`
+        computerScore+=1;
+        computerScoreBoard.textContent=`Computer = ${computerScore}`
+        currentPlay.textContent=`You lose! ${y} beats ${x}.`
     }
 }
+
 
 
 let getComputerChoice = () => {let x =Math.random()
@@ -40,9 +75,8 @@ let getComputerChoice = () => {let x =Math.random()
     }
 }
 
-
+/// Old way of running code, no longer needed
 function Game(){
-
     console.log(playRound(window.prompt("Do you choose Rock, Paper or Scissors?"),getComputerChoice()));    
     console.log("Game Over.")
 }
